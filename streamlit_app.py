7694ccb4adabf299bea5d8c8505aaa9f80fe87d0
Parmar Parth <124517644+intellectualnerd@users.noreply.github.com>
2026-01-21 15:16:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ for fruit_chosen in ingredients_list:
     st.subheader(fruit_chosen + ' Nutrition Information')
     fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
     #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
-    #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 # Insert statement
 my_insert_stmt = f"""
