5a41b19e72b98ac40a06c436dd86f7e39f5b60a5
Parmar Parth <124517644+intellectualnerd@users.noreply.github.com>
2026-01-21 15:07:17 +0530

    Modify dataframe display to use pandas conversion
    Comment out the original dataframe display and convert to pandas before displaying.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,13 @@ my_dataframe = (
     .table("smoothies.public.fruit_options")
     .select(col("FRUIT_NAME"), col("SEARCH_ON"))
 )
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
+
 # Multiselect for ingredients
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients :",
