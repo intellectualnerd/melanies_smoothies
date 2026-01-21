70acc4fd1eb4ed80b78c5f3d438f4251554c8540
Parmar Parth <124517644+intellectualnerd@users.noreply.github.com>
2026-01-21 15:18:34 +0530

    Change API call to smoothiefroot and comment fruityvice
    Updated API call to use smoothiefroot and commented out fruityvice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,8 @@ for fruit_chosen in ingredients_list:
     st.write('The search value for ', fruit_chosen, 'is', search_on, '.')
     
     st.subheader(fruit_chosen + ' Nutrition Information')
-    fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
-    #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+    #fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}" )
     sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 # Insert statement
