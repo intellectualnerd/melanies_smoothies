2f3986ddcab23b3e8d5b05a91c59d5d238ef4650
Parmar Parth <124517644+intellectualnerd@users.noreply.github.com>
2026-01-21 14:36:07 +0530

    Fix spacing in subheader for nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
 # Build ingredients string
 ingredients_string = ""
 for fruit_chosen in ingredients_list:
-    st.subheader(fruit_chosen + 'Nutrition Information')
+    st.subheader(fruit_chosen + ' Nutrition Information')
     ingredients_string += fruit_chosen + " "
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
